e4daf604c747f3eea5dbe96c4528c50bc3b4868e
Ramraj Patil <ramrajpatil24@gmail.com>
2024-08-24 15:00:51 +0530

    completed user-service

---
+++ user-service/src/main/java/com/rpd/user/service/entities/Rating.java
@@ -0,0 +1,19 @@
+package com.rpd.user.service.entities;
+
+import lombok.AllArgsConstructor;
+import lombok.Getter;
+import lombok.NoArgsConstructor;
+import lombok.Setter;
+
+@Getter
+@Setter
+@AllArgsConstructor
+@NoArgsConstructor
+public class Rating {
+
+	private String ratingId;
+	private String userId;
+	private String hotelid;
+	private int rating;
+	private String feedback;
+}
