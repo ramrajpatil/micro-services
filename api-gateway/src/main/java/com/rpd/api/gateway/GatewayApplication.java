87e1d327fc3b9c7eba53c19a797bdce0736bd100
Ramraj Patil <ramrajpatil24@gmail.com>
2024-08-25 19:11:57 +0530

    added api getway functionality

--- api-gateway/src/main/java/com/rpd/api/gateway/GatewayApplication.java
+++ api-gateway/src/main/java/com/rpd/api/gateway/GatewayApplication.java
@@ -2,12 +2,14 @@ package com.rpd.api.gateway;
 
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
 
 @SpringBootApplication
-public class ApiGatewayApplication {
+@EnableDiscoveryClient
+public class GatewayApplication {
 
 	public static void main(String[] args) {
-		SpringApplication.run(ApiGatewayApplication.class, args);
+		SpringApplication.run(GatewayApplication.class, args);
 	}
 
 }
